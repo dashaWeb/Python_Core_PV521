e56bb5f54f27c9091169b2530d6695cbb1a44ee7
Dasha <dashakonopelko@gmail.com>
2025-06-02 19:39:50 +0300

    Intro

---
+++ 01_intro.py
@@ -0,0 +1,5 @@
+#print("Hello \"World\"","\\Python\\",3, end=" --- ")
+# print("\tI L\aO\bVE" + " C++.\ndata\\base")
+# print("A","B","C","D", sep="\n")
+
+print("\033[31mstring\033[0m \ninserted \nbetween \nvalues, \ndefault \na \nspace.")
